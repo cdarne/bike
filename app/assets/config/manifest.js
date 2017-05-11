13af161ae926492397f9ade0d5aa2d22c2f59208
cdarne-darwineco <cedric@darwineco.com>
2017-05-10 22:36:56 -0400

    Fixing asset path for production

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
-//= link_tree ../componenents
+//= link_tree ../components
+//= link_tree ../../../vendor/assets/components
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
